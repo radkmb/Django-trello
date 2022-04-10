18d8aac478d5849622a1be6c4122b2779821ee2a
toritoritorina@gmail.com <>
2022-04-10 19:52:02 +0900

    カードに並び順を実装し、ドラッグアンドドロップ時に並び順が変更されるように

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
+from csv import list_dialects
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CardAdmin(admin.ModelAdmin):
+    model = Card
+    list_display = ('id', 'title', 'order')
+
+
+admin.site.register(Card, CardAdmin)
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.db.models import Prefetch, F
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, resolve_url, get_object_or_404 
 from django.urls import reverse_lazy
@@ -105,7 +106,7 @@ class CardDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy("todo:home")
 
 class HomeView(LoginRequiredMixin, ListView):
-    model = List
+    queryset = List.objects.prefetch_related(Prefetch('card_set', queryset=Card.objects.order_by('order')))
     template_name = "todo/home.html"
 
 class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
@@ -123,10 +124,40 @@ class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
 
 
 def api_card_drag(request):
-    card_pk = request.POST['cardPk']
-    list_pk = request.POST['listPk']
-    card = get_object_or_404(Card, pk=card_pk)
-    list_ = get_object_or_404(List, pk=list_pk)
+    card = get_object_or_404(Card, pk=request.POST['cardPk'])
+    list_ = get_object_or_404(List, pk=request.POST['listPk'])
+
+    # ドラッグアンドドロップしたカードが抜けた後の、下側にあったデータの並びを-1する
+    Card.objects.filter(list=card.list, order__gt=card.order).update(order=F('order')-1)
+
+    insert_target_pk = request.POST.get('insertTarget')
+    if insert_target_pk is None:
+        # 挿入先リストにまだカードがない場合か、カードとカードの間ではなくカードの親エリアの部分でドロップした
+        try:
+            # カードの間ではなくカードの親エリアの部分なら、そのlist内で一番大きい値(つまり最後に置く)
+            card.order = Card.objects.filter(list=list_).latest('order').order + 1
+        except Card.DoesNotExist:
+            # 挿入先にデータがない場合、1にする
+            card.order = 1
+
+    else:
+        # 挿入先にデータがある場合
+
+        # 挿入先の基準のカード
+        insert_target_card = get_object_or_404(Card, pk=insert_target_pk)
+
+        if request.POST['insertKind'] == 'before':
+            # そのカードの手前に挿入された
+            insert_order = insert_target_card.order
+        else:
+            # そのカードの後に挿入された
+            insert_order = insert_target_card.order + 1
+        
+        # ドラッグアンドドロップしたカードが入った後の、下側にあるデータの並びを+1する
+        Card.objects.filter(list=insert_target_card.list, order__gte=insert_order).exclude(pk=card.pk).update(order=F('order')+1)
+
+        card.order = insert_order
+
     card.list = list_
     card.save()
     return HttpResponse('ok')
